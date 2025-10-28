8adf550da1fa9a401f1f41761b042b1ce5b3570d
lukasz-mac <luka.koscik@gmail.com>
2025-10-28 22:47:05 +0100

    feat: generic type for recursive snake case object properties into camel case props WIP

--- src/db/types.ts
+++ src/db/types.ts
@@ -13,22 +13,21 @@ export type User = {
   // todo subject to change
   picture?: string,
   role: UserRole,
-  nest: {
-    propert: string
-  }
 }
 
 
 // TODO
-// transform single string with single underscore into string without underscore 
-// remove all underscores from string type
-// transform every letter after removed underscore into uppercase 
-// keep first letter in lowercase 
-// transform property of object from snake case to camelcase
-// transform everty property of object form snake to camelcase
-// transform nested object properties from snake to camelcase
+// transform single string with single underscore into string without underscore ✅
+// remove all underscores from string type ✅
+// transform every letter after removed underscore into uppercase  ✅
+// keep first letter in lowercase ✅
+// transform property of object from snake case to camelcase ✅
+// transform everty property of object form snake to camelcase ✅
+// transform nested object properties from snake to camelcase ✅
+// add case _id_another_name -> idAnotherName
 
 type Underscore = "_"
+type CamelCaseString = `_${string}`
 
 type WithoutFirstUnderscore<T extends string> = T extends `_${infer Rest}` ? Rest : T
 type SingleSnakeCaseString = "_underscore"
@@ -36,9 +35,13 @@ type SingleSnakeCaseString = "_underscore"
 type UnderscorelessString = WithoutFirstUnderscore<SingleSnakeCaseString>
 
 // Part 2
-type MultipleUnderscoresString = `multiple_underscores_test`
-type WithoutEveryUnderscore<T extends string> = T extends `${infer U}_${infer P}` ? WithoutEveryUnderscore<`${U}${Capitalize<P>}`> : T
+type CamelCasePKey<T extends string> = T extends `_${infer U}` ? Lowercase<U>  : T extends `_${infer U}_${infer P}` ? `${U}${Capitalize<CamelCasePKey<P>>}`: T extends `${infer U}_${infer P}` ? CamelCasePKey<`${U}${Capitalize<P>}`> :  T
 
-type StringWithoutAllUnderscoreCapitalized = WithoutEveryUnderscore<MultipleUnderscoresString>
+type FirstAsLowerCaseId = CamelCasePKey<"_id_private_key">
+type NoFirstUnderscoleAsLowerCaseId = CamelCasePKey<"id_private_key">
+type SimplePrivateId = CamelCasePKey<"_id">
 
+type ObjectWithCamelCaseProperties<T extends Record<string, unknown>> = {
+  [U in keyof T as CamelCasePKey<U extends string & keyof T ? U : never>] : T[U] extends Record<string, unknown> ? ObjectWithCamelCaseProperties<T[U]> : T[U]
+}
 

5d94426532df54298f1f5550dcc7e1853fecc2bb
lukasz-mac <luka.koscik@gmail.com>
2025-11-05 20:41:32 +0100

    feat: finish recursive transformation of properties in objects, clean up code

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,47 +1,29 @@
 enum UserRole {
   ADMIN = "ADMIN",
-  USER = "USER"
+  USER = "USER",
 }
 
 export type User = {
-  _id: string,
+  id: string;
   email: string;
-  username?: string,
-  password: string,
-  created_at: Date,
-  updated_at?: Date,
-  // todo subject to change
-  picture?: string,
-  role: UserRole,
+  username?: string;
+  password: string;
+  created_at: Date;
+  updated_at?: Date;
+  picture?: string;
+  role: UserRole;
 };
 
+type SnakeToCamelCase<T extends string> = T extends `_${infer U}`
+  ? SnakeToCamelCase<Lowercase<U>>
+  : T extends `_${infer U}_${infer P}`
+    ? `${Lowercase<U>}${Capitalize<SnakeToCamelCase<P>>}`
+    : T extends `${infer U}_${infer P}`
+      ? `${U}${SnakeToCamelCase<Capitalize<P>>}`
+      : T;
 
-// TODO
-// transform single string with single underscore into string without underscore ✅
-// remove all underscores from string type ✅
-// transform every letter after removed underscore into uppercase  ✅
-// keep first letter in lowercase ✅
-// transform property of object from snake case to camelcase ✅
-// transform everty property of object form snake to camelcase ✅
-// transform nested object properties from snake to camelcase ✅
-// add case _id_another_name -> idAnotherName
-
-type Underscore = "_";
-type CamelCaseString = `_${string}`;
-
-type WithoutFirstUnderscore<T extends string> = T extends `_${infer Rest}` ? Rest : T;
-type SingleSnakeCaseString = "_underscore";
-
-type UnderscorelessString = WithoutFirstUnderscore<SingleSnakeCaseString>;
-
-// Part 2
-type CamelCasePKey<T extends string> = T extends `_${infer U}` ? Lowercase<U> : T extends `_${infer U}_${infer P}` ? `${U}${Capitalize<CamelCasePKey<P>>}` : T extends `${infer U}_${infer P}` ? CamelCasePKey<`${U}${Capitalize<P>}`> : T;
-
-type FirstAsLowerCaseId = CamelCasePKey<"_id_private_key">;
-type NoFirstUnderscoleAsLowerCaseId = CamelCasePKey<"id_private_key">;
-type SimplePrivateId = CamelCasePKey<"_id">;
-
-type ObjectWithCamelCaseProperties<T extends Record<string, unknown>> = {
-  [U in keyof T as CamelCasePKey<U extends string & keyof T ? U : never>]: T[U] extends Record<string, unknown> ? ObjectWithCamelCaseProperties<T[U]> : T[U]
-}
-
+export type DeepCamelCaseKeys<T extends Record<string, unknown>> = {
+  [U in keyof T as SnakeToCamelCase<U extends string & keyof T ? U : never>]: T[U] extends Record<string, unknown>
+    ? DeepCamelCaseKeys<T[U]>
+    : T[U];
+};

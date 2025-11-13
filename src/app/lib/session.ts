adb0fedf32e540bfb821e847e2715380d9286d2e
lukasz-mac <luka.koscik@gmail.com>
2025-11-13 22:08:59 +0100

    feat: improve encryptJWT

--- src/app/lib/session.ts
+++ src/app/lib/session.ts
@@ -3,19 +3,26 @@ import * as jose from "jose";
 const JWT_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_JWT_SECRET!);
 const ISSUER = "blogspot";
 const SIGN_ALG = "HS256";
+export const EXPIRATION_15_MINUTES = "15m";
+export const EXPIRATION_7_DAYS = "7d";
 
 type JWTPayload = {
   userId: string;
 };
 
-type JWT = JWTPayload & { expiresAt: Date };
+type JWT = JWTPayload & { exp: Date };
 
-export const encryptJWT = async (jwtPayload: JWTPayload) => {
-  const authCookie = await new jose.SignJWT(jwtPayload)
+type EncryptJWTProps = {
+  payload: JWTPayload;
+  expiration?: string;
+};
+
+export const encryptJWT = async ({ payload, expiration = EXPIRATION_15_MINUTES }: EncryptJWTProps) => {
+  const authCookie = await new jose.SignJWT(payload)
     .setProtectedHeader({ alg: SIGN_ALG })
     .setIssuedAt()
     .setIssuer(ISSUER)
-    .setExpirationTime("15m")
+    .setExpirationTime(expiration)
     .sign(JWT_SIGNING_KEY);
 
   return authCookie;
@@ -27,6 +34,7 @@ export const decryptJWT = async (cookie: string) => {
       issuer: ISSUER,
     });
   } catch (err) {
+    // TODO add error handling, probably redirect
     console.error(err);
   }
 };

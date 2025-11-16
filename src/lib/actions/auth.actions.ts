fd5afd281192df724475d38f67a814d62ebb419f
lukasz-mac <luka.koscik@gmail.com>
2025-11-16 19:41:37 +0100

    refactor: change singup actions to return error message and use formdata WIP

--- src/lib/actions/auth.actions.ts
+++ src/lib/actions/auth.actions.ts
@@ -5,6 +5,7 @@ import * as passwordUtils from "@/lib/credentials/hash";
 import * as authModels from "@/models/auth.models";
 import * as JWTHelpers from "@/lib/session";
 import { cookies } from "next/headers";
+import { EXPIRATION_15_MINUTES } from "@/constants/jwt";
 
 type SignUpProps = {
   email: string;
@@ -20,54 +21,62 @@ type HandleSignUpProps = SignUpProps & {
   confirmPassword: string;
 };
 
-export const handleSignUp = async ({ email, password, confirmPassword }: HandleSignUpProps) => {
-  const parsedData = authModels.singUpSchema.safeParse({
-    email,
-    password,
-    confirmPassword,
-  });
+export const handleSignUp = async (
+  prevState: unknown,
+  data: FormData,
+  // { email, password, confirmPassword }: HandleSignUpProps
+) => {
+  const formData = {
+    email: data.get("email"),
+    password: data.get("password"),
+    confirmPassword: data.get("confirmPassword"),
+  };
+
+  const parsedData = authModels.singUpSchema.safeParse(formData);
 
   if (!parsedData.success) {
-    // todo
-    throw new Error(parsedData.error.issues.toString());
+    return {
+      message: "Incorrect account details",
+      details: parsedData.error.flatten.toString(),
+    };
   }
 
+  const { email, password } = parsedData.data;
   const userExists = await userQueries.findUserByEmail(email);
   if (userExists) {
-    // Todo add errors with codes
-    throw new Error();
+    return { message: "User with a given email already exists.", details: "" };
   }
 
-  const user = await signUp({
-    password,
-    email,
-  });
+  try {
+    const user = await signUp({
+      password,
+      email,
+    });
 
-  if (!user) {
-    throw new Error();
-  }
-
-  const { id: userId } = user;
-  const payload = { userId };
-  const [accessToken, refreshToken] = await Promise.all([
-    JWTHelpers.encryptJWT({
-      payload,
-    }),
-    JWTHelpers.encryptJWT({
+    console.log({ user });
+    const { id: userId, role } = user;
+    const payload = { userId, role };
+    // Currently only for app
+    const appAccessToken = await JWTHelpers.encryptJWT({
       payload,
       signingSecret: JWTHelpers.JWT_REFRESH_SIGNING_KEY,
-      expiration: JWTHelpers.EXPIRATION_7_DAYS,
-    }),
-  ]);
+      expiration: EXPIRATION_15_MINUTES,
+    });
 
-  const cookieStore = await cookies();
-  cookieStore.set({
-    name: "access",
-    value: accessToken,
-  });
-  cookieStore.set({
-    name: "refresh",
-    value: refreshToken,
-    httpOnly: true,
-  });
+    const cookieStore = await cookies();
+    cookieStore.set({
+      name: "session",
+      value: appAccessToken,
+      httpOnly: true,
+    });
+    return {
+      message: "",
+      details: "",
+    };
+  } catch (e) {
+    return {
+      message: "Failed to create an account.",
+      details: JSON.stringify(e),
+    };
+  }
 };

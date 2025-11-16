290537fc5c63c032c50a368c058d040d6d50f9a4
lukasz-mac <luka.koscik@gmail.com>
2025-11-16 19:38:36 +0100

    fix: correct db connection string

--- db.ts
+++ db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 const psqlPool = new Pool({
-  connectionString: process.env.POOL_CONNECTION_STRING,
+  connectionString: process.env.DB_CONNECTION_STRING,
 });
 
 export default psqlPool;

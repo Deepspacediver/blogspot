a7b594390faabf09dafbd28c2d80c86832925ec3
lukasz-mac <luka.koscik@gmail.com>
2025-11-17 22:05:04 +0100

    feat: unify handleSignup return, add strong types, replace deprecated zod method

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { handleSignUp, SignUpState } from "@/lib/actions/auth.actions";
+import React, { useActionState } from "react";
+
+const defaultSingupState = {
+  message: "",
+  fieldErrors: {},
+  prevFormState: { email: "", password: "", confirmPassword: "" },
+} satisfies SignUpState;
 
 export default function SignUp() {
+  const [errorState, signUpAction] = useActionState(handleSignUp, defaultSingupState);
+  const prevFormState = errorState.prevFormState;
   return (
     <div>
       <h1>Signup form</h1>
-      <form action="">
-        <label htmlFor="email">Enter email</label>
-        <input name="email" type="email" />
-
-        <label htmlFor="password">Enter password</label>
-        <input type="password" name="password" />
+      <form action={signUpAction}>
+        <Label htmlFor="email">Enter email</Label>
+        <Input defaultValue={prevFormState?.email || ""} name="email" type="email" />
+        <Label htmlFor="password">Enter password</Label>
+        <Input defaultValue={prevFormState?.password || ""} type="password" name="password" />
+        <Label htmlFor="confirmPassword">Confirm password</Label>
+        <Input defaultValue={prevFormState?.confirmPassword || ""} type="password" name="confirmPassword" />
+        <button>Submit</button>
       </form>
     </div>
   );

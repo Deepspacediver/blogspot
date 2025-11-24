de78e7035e54c4924dc616fa57783b624d843ba7
lukasz-mac <luka.koscik@gmail.com>
2025-11-24 21:20:41 +0100

    feat: wip navbar links

---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul className="flex gap-1">
+        <li>Home</li>
+        <li>All posts</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}

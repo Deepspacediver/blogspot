a4616eed3594ec966245b4884e278c10a17867ba
lukasz-mac <luka.koscik@gmail.com>
2025-11-26 20:25:02 +0100

    feat: improve decryptJWT, move isExpired to utils, move validateAccessToken to seperate file

--- src/features/auth-state/user-buttons.wrapper.tsx
+++ src/features/auth-state/user-buttons.wrapper.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import UserProfile from "../user/user-profile";
 import { AuthButtons } from "./auth-buttons";
-import { getSessionData } from "@/lib/auth-dal";
+import { getAppSessionData } from "@/lib/auth-dal";
 
 export default async function UserButtonsWrapper() {
-  const { user } = await getSessionData();
+  const { user } = await getAppSessionData();
   return user ? <UserProfile user={user} /> : <AuthButtons />;
 }

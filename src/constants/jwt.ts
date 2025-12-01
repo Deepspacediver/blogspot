7f1262a614dbf83cabf38ef8d844baf575ff6365
lukasz-mac <luka.koscik@gmail.com>
2025-12-01 21:59:32 +0100

    feat: move jwt constants to seperate file, refactor validateAppToken

--- src/constants/jwt.ts
+++ src/constants/jwt.ts
@@ -1,2 +1,6 @@
+import * as jose from "jose";
+
 export const EXPIRATION_15_MINUTES = "15m";
 export const EXPIRATION_7_DAYS = "7d";
+export const JWT_ACCESS_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_JWT_SECRET!);
+export const JWT_REFRESH_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_REFRESH_SIGNING_SECRET!);

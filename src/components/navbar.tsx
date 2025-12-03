31387f6e16ece5d6c03297ba215b4a095b324301
lukasz-mac <luka.koscik@gmail.com>
2025-12-03 18:59:36 +0100

    feat: add placeholder links to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
   return (
-    <nav>
-      <ul className="flex gap-1">
-        <li>Home</li>
-        <li>All posts</li>
-        <li>About</li>
+    <nav className="mx-auto">
+      <ul className="flex gap-3">
+        <Link href={"/"}>Home</Link>
+        <Link href={"/"}>All posts</Link>
+        <Link href={"/"}>About</Link>
       </ul>
     </nav>
   );

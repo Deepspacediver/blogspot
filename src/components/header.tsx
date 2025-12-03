19a1ee8d2501f0aa674702bfeb1e33691eee8bdf
lukasz-mac <luka.koscik@gmail.com>
2025-12-03 19:00:22 +0100

    feat: render header & user buttons wrapper

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Navbar from "./navbar";
 import Image from "next/image";
+import UserButtonsWrapper from "@/features/auth-state/user-buttons.wrapper";
 
 type HeaderProps = {
   children?: ReactNode;
@@ -8,9 +9,10 @@ type HeaderProps = {
 
 export default function Header({ children }: HeaderProps) {
   return (
-    <header className="h-[75px] bg-white shadow-header flex items-center">
+    <header className="h-[75px] bg-white shadow-header flex items-center gap-2 p-1">
       <Image src="/logo.svg" alt="Blogspot" width={50} height={50} />
       <Navbar />
+      <UserButtonsWrapper />
     </header>
   );
 }

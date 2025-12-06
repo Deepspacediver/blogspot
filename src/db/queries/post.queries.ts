35a1e408bb4f296b3e004d1383b19e9455b19b82
lukasz-mac <luka.koscik@gmail.com>
2025-12-06 21:46:12 +0100

    feat: update post query return type, add findPosts query

--- src/db/queries/post.queries.ts
+++ src/db/queries/post.queries.ts
@@ -26,7 +26,7 @@ export const findPost = async (id: string) => {
 type CommentsWithAuthor = Pick<UserCK & CommentCK, "username" | "email" | "pictureUrl" | "content" | "createdAt">;
 
 export const findCommentsForPost = async (id: string) => {
-  const commentsWithAuthors = await psqlPool.query<CommentsWithAuthor>(
+  const commentsWithAuthors = await psqlPool.query<OptionalReturn<CommentsWithAuthor>>(
     `
     SELECT 
       users.username,
@@ -40,6 +40,35 @@ export const findCommentsForPost = async (id: string) => {
     `,
     [id],
   );
-
   return commentsWithAuthors.rows;
+};
+
+type FindPostsReturn = Pick<
+  PostCK & UserCK,
+  "id" | "title" | "shortDescription" | "image" | "createdAt" | "email" | "pictureUrl" | "username"
+>;
+
+export const findPosts = async (cursor: string) => {
+  const { rows } = await psqlPool.query<OptionalReturn<FindPostsReturn>>(
+    `
+      SELECT 
+        posts.id,
+        posts.title,
+        posts.short_description AS "shortDescription,
+        posts.image,
+        posts.created_at aS "createdAt",
+        users.email,
+        users.username,
+        users.picture_url AS pictureUrl,
+      FROM posts 
+      JOIN users ON posts.author_id = users.id
+      WHERE 
+        posts.id > $1
+        AND posts.is_published IS TRUE
+      ORDER BY posts.id
+      `,
+    [cursor],
+  );
+
+  return rows;
 };

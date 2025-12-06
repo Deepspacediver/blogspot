a141f0149096eb1894a176767b1d0dea1ac02b05
lukasz-mac <luka.koscik@gmail.com>
2025-12-06 22:11:05 +0100

    feat: add helper for error messages

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,8 @@ export function cn(...inputs: ClassValue[]) {
 export const isExpired = (date: Date | number) => {
   return new Date(date).valueOf() - new Date().valueOf() < 0;
 };
+
+export const getErrorDetails = ({ error, defaultMessage }: { error: unknown; defaultMessage?: string }) => {
+  const genericMessage = defaultMessage || "Data request has failed.";
+  return error instanceof Error ? error.message : genericMessage;
+};

0640edaf96c37bd4d0f7530e8532029a8b1aa303
lukasz-mac <luka.koscik@gmail.com>
2025-12-10 23:27:47 +0100

    feat: add date foramt helpers

--- src/features/posts/post-preview.tsx
+++ src/features/posts/post-preview.tsx
@@ -1,9 +1,9 @@
 import { FindPostsReturn } from "@/db/queries/post.queries";
-import { format } from "date-fns";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import UserProfile from "../user/user-profile";
+import { getFormattedDateWithAttribute } from "@/lib/utils";
 
 const DEFAULT_POST_IMAGE_URL =
   "https://img.freepik.com/free-vector/cute-bee-flying-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium-vector_138676-6016.jpg?t=st=1765216630~exp=1765220230~hmac=ef55c716ffcd7870cf8931dfaf2036ec395508f47fa069b5b59a93182a90ec3d&w=1480";
@@ -14,14 +14,13 @@ type PostPreviewProps = {
 
 export default function PostPreview({ data }: PostPreviewProps) {
   const { id, title, image, createdAt, email: authorEmail, username, pictureUrl: authorPicture } = data;
-  const createdAtFormatted = format(new Date(createdAt), "MMMM d, yyyy");
-  const createdAtAttribute = format(new Date(createdAt), "yyyy-MM-dd kk:mm:ss");
+  const { attributeDate, formattedDate } = getFormattedDateWithAttribute(createdAt);
   return (
     <Link className="w-96 h-96 block" href={`/posts/${id}`}>
       <article>
         <Image src={image || DEFAULT_POST_IMAGE_URL} alt="post article" width={200} height={180} />
         <div>
-          <time dateTime={createdAtAttribute}>{createdAtFormatted}</time>
+          <time dateTime={attributeDate}>{formattedDate}</time>
           <h2>{title}</h2>
         </div>
         <div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { format } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -12,4 +13,21 @@ export const isExpired = (date: Date | number) => {
 export const getErrorDetails = ({ error, defaultMessage }: { error: unknown; defaultMessage?: string }) => {
   const genericMessage = defaultMessage || "Data request has failed.";
   return error instanceof Error ? error.message : genericMessage;
+};
+
+export const formatDateToAppConvention = (data: Date | string | number) => {
+  return format(new Date(data), "MMMM d, yyyy");
+};
+
+export const formatDateToDateTimeAttribute = (data: Date | string | number) => {
+  return format(new Date(data), "yyyy-MM-dd kk:mm:ss");
+};
+
+export const getFormattedDateWithAttribute = (data: Date | string | number) => {
+  const attributeDate = formatDateToDateTimeAttribute(data);
+  const formattedDate = formatDateToAppConvention(data);
+  return {
+    attributeDate,
+    formattedDate,
+  };
 };

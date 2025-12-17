f4914a284700f4529a2f3818e9aa58fecea109ea
lukasz-mac <luka.koscik@gmail.com>
2025-12-17 20:54:08 +0100

    feat: add access/refresh signing keys for api

--- src/constants/jwt.ts
+++ src/constants/jwt.ts
@@ -4,3 +4,6 @@ export const EXPIRATION_15_MINUTES = "15m";
 export const EXPIRATION_7_DAYS = "7d";
 export const JWT_ACCESS_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_JWT_SECRET!);
 export const JWT_REFRESH_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_REFRESH_SIGNING_SECRET!);
+
+export const JWT_API_ACCESS_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_API_JWT_SECRET!);
+export const JWT_API_REFRESH_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_APi_REFRESH_SIGNING_SECRET!);

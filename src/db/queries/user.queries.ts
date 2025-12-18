2c745cf06e4bf600641c761d1c77e565ef462dfc
lukasz-mac <luka.koscik@gmail.com>
2025-12-18 21:48:07 +0100

    feat: add updateUser & findUserById queries, add optional role to createUser

--- src/db/queries/user.queries.ts
+++ src/db/queries/user.queries.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import psqlPool from "..";
-import { OptionalReturn, UserCK } from "../types";
+import { OptionalReturn, UserCK, UserRole } from "../types";
 
 export const findUserByEmail = async (email: string) => {
   const user = await psqlPool.query<OptionalReturn<UserCK>>(
@@ -14,18 +14,56 @@ export const findUserByEmail = async (email: string) => {
   return user?.rows?.[0];
 };
 
+export const findUserById = async (id: number) => {
+  const user = await psqlPool.query<OptionalReturn<UserCK>>(
+    `
+      SELECT * FROM users WHERE id = $1;
+    `,
+    [id],
+  );
+
+  return user?.rows?.[0];
+};
+
 type CreateUserProps = {
   email: string;
   password: string;
+  role?: UserRole;
 };
-export const createUser = async ({ email, password }: CreateUserProps) => {
-  const user = await psqlPool.query<UserCK>(
+
+type UserReturn = Pick<UserCK, "id" | "role" | "username" | "email">;
+
+export const createUser = async ({ email, password, role = UserRole.USER }: CreateUserProps) => {
+  const user = await psqlPool.query<UserReturn>(
     `INSERT INTO users (
-      email, password
+      email, password, role
     ) VALUES ( 
-      $1, $2
+      $1, $2, $3
     ) RETURNING id, role, username, email;`,
-    [email, password],
+    [email, password, role],
   );
+  return user?.rows?.[0];
+};
+
+type UpdateUserProps = { id: number } & Partial<Omit<UserCK, "createdAt" | "updatedAt" | "id">>;
+
+export const updateUser = async ({ id, ...rest }: UpdateUserProps) => {
+  const columnNames = Object.entries(rest)
+    .filter(([_, val]) => val !== undefined)
+    .map(([key], i) => `${key} = $${i + 1}`)
+    .join(", ");
+  const whereIndex = columnNames.length;
+  const columnValues = [...Object.entries(rest).flatMap(([_, val]) => (!!val ? [val] : [])), id];
+
+  const user = await psqlPool.query<UserReturn>(
+    `
+    UPDATE users
+      SET ${columnNames}
+      WHERE id = $${whereIndex}
+      RETURNING id, role, username, email;
+  `,
+    columnValues,
+  );
+
   return user?.rows?.[0];
 };

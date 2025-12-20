4848e16eaa5a3fe2bf861e2ade12caac11e7afa7
lukasz-mac <luka.koscik@gmail.com>
2025-12-20 22:05:43 +0100

    refactor: rename env vars

--- src/constants/jwt.ts
+++ src/constants/jwt.ts
@@ -2,8 +2,8 @@ import * as jose from "jose";
 
 export const EXPIRATION_15_MINUTES = "15m";
 export const EXPIRATION_7_DAYS = "7d";
-export const JWT_ACCESS_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_JWT_SECRET!);
-export const JWT_REFRESH_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_REFRESH_SIGNING_SECRET!);
+export const JWT_ACCESS_SIGNING_KEY = jose.base64url.decode(process.env.APP_JWT_SIGNING_SECRET!);
+export const JWT_REFRESH_SIGNING_KEY = jose.base64url.decode(process.env.APP_JWT_REFRESH_SECRET!);
 
-export const JWT_API_ACCESS_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_API_JWT_SECRET!);
-export const JWT_API_REFRESH_SIGNING_KEY = jose.base64url.decode(process.env.BLOGSPOT_APi_REFRESH_SIGNING_SECRET!);
+export const JWT_API_ACCESS_SIGNING_KEY = jose.base64url.decode(process.env.API_JWT_SIGNING_SECRET!);
+export const JWT_API_REFRESH_SIGNING_KEY = jose.base64url.decode(process.env.API_JWT_REFRESH_SECRET!);

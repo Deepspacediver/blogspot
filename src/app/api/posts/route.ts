37fa7281ce095a4139e94bb48ef9ff111b5837b1
lukasz-mac <luka.koscik@gmail.com>
2025-12-24 21:25:26 +0100

    feat: api/posts endpoints

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,64 @@
+import * as postQueries from "@/db/queries/post.queries";
+import { UserRole } from "@/db/types";
+import { protectedAction } from "@/lib/session";
+import { APIResponse } from "@/lib/utils";
+import * as postModels from "@/models/post.models";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  return protectedAction(async ({ body }) => {
+    const parsedData = postModels.findPostsSchema.safeParse(body);
+    if (!parsedData.success) {
+      return APIResponse({
+        data: {
+          message: "Invalid request data",
+        },
+        status: 400,
+      });
+    }
+
+    const posts = await postQueries.findPosts({ cursor: parsedData.data.cursor });
+    return APIResponse({
+      data: { posts },
+    });
+  }, req);
+}
+
+export async function POST(req: NextRequest) {
+  return protectedAction(async ({ payload, body }) => {
+    const parsedData = postModels.createPostSchema.safeParse(body);
+    if (!parsedData.success) {
+      return APIResponse({
+        data: {
+          message: "Invalid request data",
+        },
+        status: 400,
+      });
+    }
+
+    if (payload.role === UserRole.USER) {
+      return APIResponse({
+        data: {
+          message: "Forbidden",
+        },
+        status: 403,
+      });
+    }
+    const { title, content, shortDescription, image, isPublished } = parsedData.data;
+    await postQueries.createPost({
+      authoId: payload.userId,
+      title,
+      content,
+      image,
+      shortDescription,
+      isPublished,
+    });
+
+    return APIResponse({
+      data: {
+        message: "Created",
+      },
+      status: 201,
+    });
+  }, req);
+}

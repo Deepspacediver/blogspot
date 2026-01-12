6c045a6a08b55bbb26c979fd234071131f5ca917
lukasz-mac <luka.koscik@gmail.com>
2026-01-12 21:57:10 +0100

    feat: add signout api route

---
+++ src/app/api/auth/signout/route.ts
@@ -0,0 +1,19 @@
+import { APIResponse, clearJWTCookies } from "@/lib/utils";
+import { cookies } from "next/headers";
+
+export async function POST() {
+  const cookieStore = await cookies();
+  const accessCookie = cookieStore.get("access");
+  const refreshCookie = cookieStore.get("refresh");
+  if (!accessCookie || !refreshCookie) {
+    return APIResponse({
+      data: "Not logged in",
+      status: 400,
+    });
+  }
+  clearJWTCookies({ cookieStore, isAPI: true });
+
+  return APIResponse({
+    data: "ok",
+  });
+}

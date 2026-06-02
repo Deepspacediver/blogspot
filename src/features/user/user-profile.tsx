2fd664ad6712255abc5497f2a504e404d4c57266
lukasz-mac <luka.koscik@gmail.com>
2026-06-02 21:31:40 +0200

    feat: add optional picture flag for <UserProfile/>

--- src/features/user/user-profile.tsx
+++ src/features/user/user-profile.tsx
@@ -10,19 +10,22 @@ type UserProfileProps = {
   };
   className?: string;
   children?: ReactNode;
+  withPicture?: boolean;
 };
 
-export default function UserProfile({ user, className, children }: UserProfileProps) {
+export default function UserProfile({ user, className, children, withPicture = true }: UserProfileProps) {
   const { email, username, pictureUrl } = user;
   return (
     <div className={clsx("flex gap-1 items-center", className)}>
-      <div className={clsx("rounded-full overflow-hidden shrink-0")}>
-        {pictureUrl ? (
-          <Image src={pictureUrl} width={24} height={24} alt="user picture" />
-        ) : (
-          <User className="size-10 p-1 shrink-" />
-        )}
-      </div>
+      {withPicture && (
+        <div className={clsx("rounded-full overflow-hidden shrink-0")}>
+          {pictureUrl ? (
+            <Image src={pictureUrl} width={24} height={24} alt="user picture" />
+          ) : (
+            <User className="size-10 p-1 shrink-" />
+          )}
+        </div>
+      )}
       <div className="flex flex-col truncate">
         <p className="flex-1 truncate">{username || email}</p>
         {children}

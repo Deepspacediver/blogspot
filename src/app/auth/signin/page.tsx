621572abb76ac73ad966d468fae164f1e72a576c
lukasz-mac <luka.koscik@gmail.com>
2026-09-03 18:53:42 +0200

    feat(forms): add support for mutliple error messages, add missing FormMessages in forms

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
+import { FormMessage, Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { defaultSignInState } from "@/constants/form-states";
 import { handleSignIn } from "@/lib/actions/auth.actions";
@@ -9,14 +9,17 @@ import React, { useActionState } from "react";
 export default function SignIn() {
   const [errorState, signInAction] = useActionState(handleSignIn, defaultSignInState);
   const prevFormState = errorState.prevFormState;
+  const errors = errorState.fieldErrors;
   return (
     <div>
       <h1>Sign in form</h1>
       <form action={signInAction}>
         <Label htmlFor="email">Enter email</Label>
         <Input defaultValue={prevFormState?.email || ""} name="email" type="email" />
+        <FormMessage messages={errors.email} />
         <Label htmlFor="password">Enter password</Label>
         <Input defaultValue={prevFormState?.password || ""} type="password" name="password" />
+        <FormMessage messages={errors.password} />
         <button>Submit</button>
       </form>
     </div>
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
+import { FormMessage, Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { defaultSignupState } from "@/constants/form-states";
 import { handleSignUp } from "@/lib/actions/auth.actions";
@@ -9,16 +9,20 @@ import React, { useActionState } from "react";
 export default function SignUp() {
   const [errorState, signUpAction] = useActionState(handleSignUp, defaultSignupState);
   const prevFormState = errorState.prevFormState;
+  const errors = errorState.fieldErrors;
   return (
     <div>
       <h1>Signup form</h1>
       <form action={signUpAction}>
         <Label htmlFor="email">Enter email</Label>
         <Input defaultValue={prevFormState?.email || ""} name="email" type="email" />
+        <FormMessage messages={errors.email} />
         <Label htmlFor="password">Enter password</Label>
         <Input defaultValue={prevFormState?.password || ""} type="password" name="password" />
+        <FormMessage messages={errors.password} />
         <Label htmlFor="confirmPassword">Confirm password</Label>
         <Input defaultValue={prevFormState?.confirmPassword || ""} type="password" name="confirmPassword" />
+        <FormMessage messages={errors.confirmPassword} />
         <button>Submit</button>
       </form>
     </div>
